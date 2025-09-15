abe8c3ca20399e10c3218b92231bf6ed4cac38d8
헤빈 <hyebeenbyeon@gmail.com>
2025-09-15 14:29:45 +0900

    새로운 기획에 맞춘 새로운 프로젝트!

---
+++ feelem/src/main/java/com/feelem/server/FeelemApplication.java
@@ -0,0 +1,13 @@
+package com.feelem.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeelemApplication.class, args);
+  }
+
+}
---
+++ feelem/src/test/java/com/feelem/server/FeelemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelem.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
